4ce87d81593bc305f6e960d4abf716b4d189ff69
JuliaPo <nefigovayaa@gmail.com>
2016-10-06 16:10:02 +0400

    Update project

---
+++ Calc/Domain/Models/History.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class History
+    {
+        public virtual Guid Id { get; set; }
+
+        [Required]
+        [Display(Name ="Первая переменная")]
+        public virtual int X { get; set; }
+
+        [Required]
+        [Display(Name = "Первая переменная")]
+        public virtual int Y { get; set; }
+
+        [Required]
+        [Display(Name = "Результат")]
+        public virtual string Operation { get; set; }
+
+        [Required]
+        [Display(Name = "Операция")]
+        public virtual int Result { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата создания")]
+        public virtual DateTime CreationDate { get; set; }
+
+    }
+}
